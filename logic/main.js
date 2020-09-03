c1709991c0cdd3efe82938c9c39e5430a189f4d0
Ming Hsieh <minghsiehlo@gmail.com>
2020-09-02 19:00:25 -0500

    colors v2

--- logic/main.js
+++ logic/main.js
@@ -3,4 +3,13 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click',() => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
+    btnSwitchMobile.classList.toggle('active');
+})
+
+const btnSwitchMobile = document.querySelector('#switch-mobile');
+
+btnSwitchMobile.addEventListener('click',() => {
+    document.body.classList.toggle('dark');
+    btnSwitchMobile.classList.toggle('active');
+    btnSwitch.classList.toggle('active');
 })
